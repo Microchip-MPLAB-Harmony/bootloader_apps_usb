886899de0bf507a6206640fecdedbcb75af6788c
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2023-12-20 17:30:45 +0530

    [USB] Regenerated demo for usb_host_msd_bootloader_bootloader_pic32mk_mcm_curiosity_pro
    MH3-88692

--- apps/usb_host_msd_bootloader/bootloader/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
+++ apps/usb_host_msd_bootloader/bootloader/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
@@ -48,7 +48,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -119,6 +118,11 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 7.2 */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
 
 
 // *****************************************************************************
@@ -139,51 +143,51 @@ SYSTEM_OBJECTS sysObj;
  ******************************************************/
  
 
-uint8_t __attribute__((aligned(512))) endPointTable0[DRV_USBFS_ENDPOINTS_NUMBER * 32];
+static uint8_t __attribute__((aligned(512))) endPointTable0[DRV_USBFS_ENDPOINTS_NUMBER * 32];
 
-void DRV_USB_VBUSPowerEnable0(uint8_t port, bool enable)
+static void DRV_USB_VBUSPowerEnable0(uint8_t port, bool enable)
 {
-	/* Note: USB Host applications should have a way for Enabling/Disabling the 
-	   VBUS. Applications can use a GPIO to turn VBUS on/off through a switch. 
-	   In MHC Pin Settings select the pin used as VBUS Power Enable as output and 
-	   name it to "VBUS_AH". If you a see a build error from this function either 
-	   you have not configured the VBUS Power Enable in MHC pin settings or the 
-	   Pin name entered in MHC is not "VBUS_AH". */ 
+    /* Note: USB Host applications should have a way for Enabling/Disabling the 
+       VBUS. Applications can use a GPIO to turn VBUS on/off through a switch. 
+       In MHC Pin Settings select the pin used as VBUS Power Enable as output and 
+       name it to "VBUS_AH". If you a see a build error from this function either 
+       you have not configured the VBUS Power Enable in MHC pin settings or the 
+       Pin name entered in MHC is not "VBUS_AH". */ 
     if (enable == true)
-	{
-		/* Enable the VBUS */
-		VBUS_AH_PowerEnable();
-	}
-	else
-	{
-		/* Disable the VBUS */
-		VBUS_AH_PowerDisable();
-	}
+    {
+        /* Enable the VBUS */
+        VBUS_AH_PowerEnable();
+    }
+    else
+    {
+        /* Disable the VBUS */
+        VBUS_AH_PowerDisable();
+    }
 }
 
-const DRV_USBFS_INIT drvUSBFSInit0 =
+static const DRV_USBFS_INIT drvUSBFSInit0 =
 {
-	 /* Assign the endpoint table */
+     /* Assign the endpoint table */
     .endpointTable= endPointTable0,
-	/* Interrupt Source for USB module */
-	.interruptSource = INT_SOURCE_USB_1 ,
+    /* Interrupt Source for USB module */
+    .interruptSource = INT_SOURCE_USB_1 ,
    
-	/* USB Controller to operate as USB Host */
+    /* USB Controller to operate as USB Host */
     .operationMode = DRV_USBFS_OPMODE_HOST,
-	
-	.operationSpeed = USB_SPEED_FULL,
+    
+    .operationSpeed = USB_SPEED_FULL,
  
-	/* Stop in idle */
+    /* Stop in idle */
     .stopInIdle = false,
-	
-	    /* Suspend in sleep */
+    
+        /* Suspend in sleep */
     .suspendInSleep = false,
     /* Identifies peripheral (PLIB-level) ID */
     .usbID = USB_ID_1,
-	
-	/* USB Host Power Enable. USB Driver uses this function to Enable the VBUS */ 
-	.portPowerEnable = DRV_USB_VBUSPowerEnable0,
-	
+    
+    /* USB Host Power Enable. USB Driver uses this function to Enable the VBUS */ 
+    .portPowerEnable = DRV_USB_VBUSPowerEnable0,
+    
     /* Root hub available current in milliamperes */
     .rootHubAvailableCurrent = 500,
 
@@ -292,7 +296,7 @@ static const SYS_TIME_INIT sysTimeInitData =
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -306,8 +310,11 @@ static const SYS_TIME_INIT sysTimeInitData =
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+    /* MISRA C-2012 Rule 11.6 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_11_6_DR_1 */
     /* Start out with interrupts disabled before configuring any modules */
-    __builtin_disable_interrupts();
+    (void)__builtin_disable_interrupts();
 
     CLK_Initialize();
 
@@ -329,11 +336,16 @@ void SYS_Initialize ( void* data )
         run_Application(APP_JUMP_ADDRESS);
     }
 
+
     NVM_Initialize();
 
     CORETIMER_Initialize();
 	BSP_Initialize();
 
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 
     /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -  
     H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
@@ -342,16 +354,17 @@ void SYS_Initialize ( void* data )
     
     /* MISRAC 2012 deviation block end */
 
-	/* Initialize USB Driver */ 
-    sysObj.drvUSBFSObject0 = DRV_USBFS_Initialize(DRV_USBFS_INDEX_0, (SYS_MODULE_INIT *) &drvUSBFSInit0);	
+    /* Initialize USB Driver */ 
+    sysObj.drvUSBFSObject0 = DRV_USBFS_Initialize(DRV_USBFS_INDEX_0, (SYS_MODULE_INIT *) &drvUSBFSInit0);    
 
     /* Initialize the USB Host layer */
-    sysObj.usbHostObject0 = USB_HOST_Initialize (( SYS_MODULE_INIT *)& usbHostInitData );	
+    sysObj.usbHostObject0 = USB_HOST_Initialize (( SYS_MODULE_INIT *)& usbHostInitData );    
 
     /*** File System Service Initialization Code ***/
     (void) SYS_FS_Initialize( (const void *) sysFSInit );
 
 
+    /* MISRAC 2012 deviation block end */
     APP_Initialize();
 
 
@@ -361,4 +374,5 @@ void SYS_Initialize ( void* data )
     (void)__builtin_enable_interrupts();
 
 
+    /* MISRAC 2012 deviation block end */
 }
