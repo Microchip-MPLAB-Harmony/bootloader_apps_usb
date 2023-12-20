d975d49e0e9590190989b0d15f7d791f91647c6b
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2023-12-20 17:46:17 +0530

    [USB] Regenerated demo for usb_device_hid_bootloader_test_app_sam_l21_xpro
    MH3-88689

--- apps/usb_device_hid_bootloader/test_app/firmware/src/config/sam_l21_xpro/definitions.h
+++ apps/usb_device_hid_bootloader/test_app/firmware/src/config/sam_l21_xpro/definitions.h
@@ -70,6 +70,12 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML21J18B"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML21"
+
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
 
