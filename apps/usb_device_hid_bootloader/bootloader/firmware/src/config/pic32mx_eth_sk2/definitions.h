20d622bed183c2a54a875b03e6e0d19256471def
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2025-06-10 11:22:53 +0530

    [USB_DEVICE_HID_BOOTLOADER] Regenerated demo for usb_device_hid_bootloader__bootloader__firmware__pic32mx_eth_sk2.X
    MH3-113413

--- apps/usb_device_hid_bootloader/bootloader/firmware/src/config/pic32mx_eth_sk2/definitions.h
+++ apps/usb_device_hid_bootloader/bootloader/firmware/src/config/pic32mx_eth_sk2/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -48,9 +48,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "bsp/bsp.h"
 #include "usb/usb_device_hid.h"
 #include "usb/usb_hid.h"
+#include "bsp/bsp.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
@@ -75,13 +75,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MX795F512L"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MX1156"
-#define DEVICE_SERIES		 "PIC32MX"
+#define DEVICE_NAME          "PIC32MX795F512L"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MX1156"
+#define DEVICE_SERIES        "PIC32MX"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 80000000
+#define CPU_CLOCK_FREQUENCY 80000000U
 
 // *****************************************************************************
 // *****************************************************************************
