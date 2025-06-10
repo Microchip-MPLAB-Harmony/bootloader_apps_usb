fb054365c271b5ce74018174236bbe83f48ec02d
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2025-06-10 11:35:45 +0530

    [USB_DEVICE_HID_BOOTLOADER] Regenerated demo for usb_device_hid_bootloader__bootloader__firmware__sam_e70_xult.X
    MH3-113427

--- apps/usb_device_hid_bootloader/bootloader/firmware/src/config/sam_e70_xult/driver/usb/usbhsv1/src/drv_usbhsv1_device.c
+++ apps/usb_device_hid_bootloader/bootloader/firmware/src/config/sam_e70_xult/driver/usb/usbhsv1/src/drv_usbhsv1_device.c
@@ -291,7 +291,7 @@ USB_SPEED DRV_USBHSV1_DEVICE_CurrentSpeedGet
 }/* end of DRV_USBHSV1_DEVICE_CurrentSpeedGet() */
 
 // *****************************************************************************
-/* MISRA C-2012 Rule 5.1 deviated:4 Deviation record ID -  H3_MISRAC_2012_R_5_1_DR_1 */
+/* MISRA C-2012 Rule 5.1 deviated:4 Deviation record ID -  H3_USB_MISRAC_2012_R_5_1_DR_1 */
 /* Function:
       void DRV_USBHSV1_DEVICE_RemoteWakeupStart(DRV_HANDLE handle)
 
@@ -352,7 +352,7 @@ void DRV_USBHSV1_DEVICE_RemoteWakeupStop(DRV_HANDLE handle)
 }/* end of DRV_USBHSV1_DEVICE_RemoteWakeupStop() */
 
 // *****************************************************************************
-/* MISRA C-2012 Rule 10.4 False Positive:8 Deviation record ID -  H3_MISRAC_2012_R_10_4_DR_1 */
+/* MISRA C-2012 Rule 10.4 False Positive:8 Deviation record ID -  H3_USB_MISRAC_2012_R_10_4_DR_1 */
 
 /* Function:
       void DRV_USBHSV1_DEVICE_Attach(DRV_HANDLE handle)
@@ -600,7 +600,7 @@ uint16_t DRV_USBHSV1_DEVICE_SOFNumberGet(DRV_HANDLE handle)
 }/* end of DRV_USBHSV1_DEVICE_SOFNumberGet() */
 
 // *****************************************************************************
-/* MISRA C-2012 Rule 11.3 deviated:31 Deviation record ID -  H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.3 deviated:31 Deviation record ID -  H3_USB_MISRAC_2012_R_11_3_DR_1 */
 /* Function:
     void F_DRV_USBHSV1_DEVICE_IRPQueueFlush
     (
@@ -2003,7 +2003,7 @@ USB_ERROR DRV_USBHSV1_DEVICE_IRPSubmit
                             if(USBHS_DEVEPTISR_RXOUTI_Msk == (USBHS_DEVEPTISR_RXOUTI_Msk & usbID->USBHS_DEVEPTISR[endpoint]))
                             {
                                 /* Data is already available in the FIFO */
-                                byteCount = (uint16_t)(usbID->USBHS_DEVEPTISR[endpoint] & USBHS_DEVEPTISR_BYCT_Msk) >> USBHS_DEVEPTISR_BYCT_Pos;
+                                byteCount = (uint16_t)( (usbID->USBHS_DEVEPTISR[endpoint] & USBHS_DEVEPTISR_BYCT_Msk) >> USBHS_DEVEPTISR_BYCT_Pos);
 
                                 /* Get FIFO Address */
                                 ptr = (uint8_t *) & ((volatile uint8_t (*)[0x8000])USBHSV1_RAM_ADDR)[endpoint];
